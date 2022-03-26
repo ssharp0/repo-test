b8614bc53a41cb5bd2b02373cf9662a9e64eb3a3
Scott Harp <87046544+ssharp0@users.noreply.github.com>
2022-03-25 23:58:04 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log()
+
+const express = require('express')
+const { join } = require('path')
+
+// app
+const app = express()
+
+// middleware
+app.use(express.static(join(__dirname, 'client', 'build')))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())

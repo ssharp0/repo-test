cd8e3e8f07026fbc3fc29a656bf91344b8c447c6
Scott Harp <87046544+ssharp0@users.noreply.github.com>
2022-08-27 10:09:28 -0700

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,26 @@
+import './App.css';
+import { Container, AppBar, Typography, Grow, Grid } from '@mui/material'
+import { makeStyles } from "@mui/styles";
 
+const useStyles = makeStyles(() => ({
+  container: {
+    marginTop: '50px',
+  },
+  heading: {
+   color: '#F2FDFF !important'
+  }
+}))
+
+function App() {
+
+  const classes = useStyles()
+
+  return (
+    <div className="App">
+      <Container maxWidth="lg" className={classes.container}>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
